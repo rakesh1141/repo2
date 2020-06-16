17d098e15bb1256d5e6d236e62f74c7ac02e9ab1
rakesh1141 <rakesh.1141@gmail.com>
2020-06-16 15:20:21 +0530

    all files

---
+++ src/main/java/com/pack1/Main.java
@@ -0,0 +1,8 @@
+package com.pack1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/com/pack1/Test.java
@@ -0,0 +1,7 @@
+package com.pack1;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
