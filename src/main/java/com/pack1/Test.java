140b966cfe718f41cf008d00ac924d98cc886343
rakesh1141 <rakesh.1141@gmail.com>
2020-06-16 21:11:13 +0530

    updated test.java from master

--- src/main/java/com/pack1/Test.java
+++ src/main/java/com/pack1/Test.java
@@ -3,6 +3,7 @@ package com.pack1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("updayed");
+        System.out.println("master updated");
 
     }
 }
